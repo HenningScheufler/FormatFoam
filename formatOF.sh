6fe5cc36ef118759b85becf080a65f10cedf7ed7
Henning Scheufler <henning.scheufler@web.de>
2024-04-09 19:40:21 +0200

    update formatOF so lemon.c is not updated

--- formatOF.sh
+++ formatOF.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
-find . -iname *.C -o -iname *.H | xargs clang-format -i --style=file:"$1"
+find applications -iname *.C -o -iname *.H | xargs clang-format -i --style=file:"$1"
+find etc -iname *.C -o -iname *.H | xargs clang-format -i --style=file:"$1"
+find modules -iname *.C -o -iname *.H | xargs clang-format -i --style=file:"$1"
+find src -iname *.C -o -iname *.H | xargs clang-format -i --style=file:"$1"
+find tutorials -iname *.C -o -iname *.H | xargs clang-format -i --style=file:"$1"
